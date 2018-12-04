24ff6ed742723da19143ec132444b83075e5021b
unknown <ebaidoo79@gmail.com>
2018-12-04 22:41:33 +0000

    create farmer forms and farmer list in progress

---
+++ app/src/main/java/com/example/khoby/tcntracker/FarmerProfiles.java
@@ -0,0 +1,13 @@
+package com.example.khoby.tcntracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FarmerProfiles extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farmer_profiles);
+    }
+}
